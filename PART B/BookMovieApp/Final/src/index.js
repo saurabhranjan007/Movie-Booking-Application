5ef2e0dbb27458029327d3e82d489b8a0c80bb2d
Saurabh Ranjan <saurabhranjanw@gmail.com>
2022-01-16 21:45:35 +0530

    PART-A

--- PART B/BookMovieApp/Final/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import 'typeface-roboto';
-import registerServiceWorker from './registerServiceWorker';
-import Controller from './screens/Controller';
-
-ReactDOM.render(<Controller />, document.getElementById('root'));
-registerServiceWorker();
